9a4767cf9eca408dca2410846824d69d6ade7ad7
VitalikPidburtnyi <vitalikpidburtnyi24@gmail.com>
2022-10-05 23:08:35 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int number;
+        Console.WriteLine("Enter a number:");
+        while (true)
+        {
+            try
+            {
+                number = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("You entered number " + number);
+                break;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Not a number");
+            }
+        }
+    }
+}
